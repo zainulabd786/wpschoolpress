12b9742411272343c393b61b3d7a7468a187ee5e
Zainul Abideen <zainulabd786@gmail.com>
2018-03-19 10:37:28 +0530

    Added styles to the printing document

--- includes/wpsp-depositFeesForm.php
+++ includes/wpsp-depositFeesForm.php
@@ -272,10 +272,10 @@
 												<div class="invoice-header">
 													<div class="invoice-header-logo-name row">
 
-														<div class="invoice-header-logo col-md-3">
+														<div class="invoice-header-logo col-md-3 col-print-3">
 															<img src="<?php if(!empty($school_logo)) echo $school_logo; ?>" height=90 width=90>
 														</div>
-														<div class="invoice-header-name col-md-9">
+														<div class="invoice-header-name col-md-9 col-print-9">
 															<h2><?php if(!empty($school_name)) echo $school_name; ?></h2>
 														</div>
 
