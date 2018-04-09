128fa336f6cae8a51432176e63d88f1a6a2c845f
Zainul Abideen <zainulabd786@gmail.com>
2018-04-09 12:40:02 +0530

    fixed Error in  payment history date filter

--- includes/wpsp-paymentHistory.php
+++ includes/wpsp-paymentHistory.php
@@ -1,5 +1,5 @@
-<?php 
-	global $current_user;	
+<?php
+	global $current_user;
 	$role		=	 $current_user->roles;
 	$cuserId	=	 $current_user->ID;
 ?>
@@ -21,22 +21,22 @@
 								<form name="StudentClass" id="StudentClass" method="post" action="" class="class-filter">
 									<label><?php _e( 'Select Class Name', 'WPSchoolPress' ); ?></label>
 									<select name="ClassID" id="ClassID" class="form-control">
-										<?php 
-										$sel_classid	=	isset( $_POST['ClassID'] ) ? $_POST['ClassID'] : '';										
+										<?php
+										$sel_classid	=	isset( $_POST['ClassID'] ) ? $_POST['ClassID'] : '';
 										$class_table	=	$wpdb->prefix."wpsp_class";
 										$sel_class		=	$wpdb->get_results("select cid,c_name from $class_table Order By cid ASC");
 										foreach( $sel_class as $classes ) {
-										?> 
+										?>
 											<option value="<?php echo $classes->cid;?>" <?php if($sel_classid==$classes->cid) echo "selected"; ?>><?php echo $classes->c_name;?></option>
 										<?php } ?>
 										 <?php if ( in_array( 'administrator', $role ) ) { ?>
 											<option value="all" <?php if($sel_classid=='all') echo "selected"; ?>><?php _e( 'All', 'WPSchoolPress' ); ?></option>
 										 <?php } ?>
 									</select>
-								</form>								
+								</form>
 							</div>
 							<div class="col-md-8 col-sm-12 col-lg-8 ">
-									
+
 								<form style="float: left" action="" method="post" name="date-filter-form" id="date-filter-form" class="form-inline">
 
 									<div class="form-group">
@@ -73,13 +73,13 @@
 								</thead>
 								<tbody>
 									<?php
-									$student_table	=	$wpdb->prefix."wpsp_student";							
+									$student_table	=	$wpdb->prefix."wpsp_student";
 									$users_table	=	$wpdb->prefix."users";
 									$fee_rec_table	=	$wpdb->prefix."wpsp_fees_receipts";
 									$fee_record_table	=	$wpdb->prefix."wpsp_fees_payment_record";
 									$class_table = $wpdb->prefix."wpsp_class";
 									$dues_table = $wpdb->prefix."wpsp_fees_dues";
-									$class_id='';						
+									$class_id='';
 									if( isset($_POST['ClassID'] ) ) {
 										$class_id=$_POST['ClassID'];
 									}else if( !empty( $sel_class ) ) {
@@ -95,14 +95,14 @@
 									if(isset($_POST["from-date"]) && isset($_POST["to-date"])){
 										$from_date = date("Y-m-d", strtotime($_POST["from-date"]));
 										$to_date = date("Y-m-d", strtotime($_POST["to-date"]));
-										$date_query = "AND f.date BETWEEN '$from_date' AND '$to_date'";
+										$date_query = "AND a.date BETWEEN '$from_date' AND '$to_date'";
 										$showing_records = "Showing Records From $from_date To $to_date"; ?>
 										<script type="text/javascript">
 											document.getElementById("record-dates").innerHTML = "<?php echo $showing_records; ?>";
 										</script><?php
 									}
 									else{
-										$date_query = "";	
+										$date_query = "";
 										$showing_records = "";
 									}
 									$filter_sql = $wpdb->get_results("SELECT SUM(a.amount) AS total_payments FROM $fee_record_table a, $student_table s, $class_table c WHERE s.wp_usr_id=a.uid AND c.cid=s.class_id $classquery $date_query");
@@ -112,17 +112,17 @@
 										</script> <?php
 									}
 									$students	=	$wpdb->get_results("select * from $student_table s, $fee_rec_table f, $class_table c where s.wp_usr_id = f.uid AND c.cid = s.class_id $classquery $date_query order by f.slip_no desc");
-									
+
 									$plugins_url=plugins_url();
 									$teacherId = '';
 									$currentSelectClass =	isset($_POST['ClassID']) ? $_POST['ClassID'] : $sel_class[0]->cid;
-									if( $currentSelectClass != 'all' ) 
+									if( $currentSelectClass != 'all' )
 										$teacherId	=	$wpdb->get_var("select teacher_id from $class_table WHERE cid=$currentSelectClass");
 									$key =0;
 									foreach($students as $stinfo)
-									{	
-										$key++;	
-										$amount = $stinfo->adm+$stinfo->ttn+$stinfo->trans+$stinfo->ann+$stinfo->rec;					
+									{
+										$key++;
+										$amount = $stinfo->adm+$stinfo->ttn+$stinfo->trans+$stinfo->ann+$stinfo->rec;
 									?>
 										<tr>
 											<td>
@@ -132,24 +132,24 @@
 											</td>
 											<td><?php echo $stinfo->slip_no;?></td>
 											<td>
-												<?php 
+												<?php
 												echo $stinfo->date;
 												?>
 											</td>
 											<td>
-												<?php 
+												<?php
 													echo $stinfo->s_regno;
 												?>
-													
+
 											</td>
 											<td>
-												<?php  
+												<?php
 													$mname = $stinfo->s_mname;
 										            $lname = $stinfo->s_lname;
-													echo $stinfo->s_fname .' '. $mname .' '.  $lname; 
+													echo $stinfo->s_fname .' '. $mname .' '.  $lname;
 												?>
 											</td>
-											<td><?php 
+											<td><?php
 												echo $stinfo->p_fname." ".$stinfo->p_lname;
 											?></td>
 											<td><?php echo $stinfo->c_name; ?></td>
@@ -157,8 +157,8 @@
 												<?php echo "<i class='fa fa-inr'></i>".number_format($amount)."/-"; ?>
 											</td>
 											<td>
-												<a class="view-transaction" id="<?php echo $stinfo->slip_no; ?>" title="View"><i class="fa fa-eye btn btn-success"></i></a> 
-												<button class="btn btn-basic view-invoice" id="<?php echo $stinfo->slip_no; ?>" type="button">Invoice</button> 	
+												<a class="view-transaction" id="<?php echo $stinfo->slip_no; ?>" title="View"><i class="fa fa-eye btn btn-success"></i></a>
+												<button class="btn btn-basic view-invoice" id="<?php echo $stinfo->slip_no; ?>" type="button">Invoice</button>
 											</td>
 										</tr>
 									<?php
@@ -167,10 +167,10 @@
 								</tbody>
 								<tfoot>
 								  <tr>
-									<th><?php if ( in_array( 'administrator', $role ) ) { } 
+									<th><?php if ( in_array( 'administrator', $role ) ) { }
 										else echo 'Sr. No'; ?></th>
-									<th>Slip No.</th>	
-									<th>Date & Time</th>							
+									<th>Slip No.</th>
+									<th>Date & Time</th>
 									<th>Registration Number</th>
 									<th>Full Name</th>
 									<th>Parent</th>
@@ -180,9 +180,9 @@
 								  </tr>
 								</tfoot>
 							  </table>
-						</div>					  
+						</div>
 					</div><!-- /.box-body -->
-				</div><!-- /.box -->				
+				</div><!-- /.box -->
 			</div>
 		</div>
 	</section>
