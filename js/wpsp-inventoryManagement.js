c0fbee10a930da6e2fe8bb62bf70eab592858c9c
Zainul Abideen <zainulabd786@gmail.com>
2018-07-15 15:44:46 +0530

    Fixed blank inputs in inventory modules

--- js/wpsp-inventoryManagement.js
+++ js/wpsp-inventoryManagement.js
@@ -5,7 +5,13 @@ $(document).ready(function() {
  	 });
 
   	$("body").on('change', ".add-item-inp", function() {
-    	$.post(ajax_url, { action: "add_item_to_inv_master_table", item: $(this).val() }, function(data){ $(".items-dropdown select").html(data); });
+      if ($(this).val().replace(/\s/g, '').length) {
+        $.post(ajax_url, { action: "add_item_to_inv_master_table", item: $(this).val() }, function(data){ $(".items-dropdown select").html(data); });
+      }
+      else{
+        alert("Your input must contain a proper item name");
+        $(this).val("");
+      }
   	});
 
   	$(".submit-btn").click(function(){
